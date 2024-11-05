cc1e050050cae0b73295701059b11824fcfcb260
Akash-Gupta-yash <ag.office00@gmail.com>
2024-11-06 00:06:25 +0530

    Initial Commit

---
+++ FactoryPatternDemo/FactoryPatternDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryPatternDemoApp.swift
+//  FactoryPatternDemo
+//
+//  Created by Deeksha Verma on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FactoryPatternDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
